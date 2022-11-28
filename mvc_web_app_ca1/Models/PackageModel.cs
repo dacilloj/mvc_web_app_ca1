70c2a033f401198001750897a23a291b01718ac6
dacillo <johncarlodacillo123@gmail.com>
2022-11-28 18:35:18 +0000

    added packge model

---
+++ mvc_web_app_ca1/Models/PackageModel.cs
@@ -0,0 +1,14 @@
+﻿namespace mvc_web_app_ca1.Models
+{
+    public class PackageModel
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+
+        public double Weight { get; set; } 
+        public double Lenght { get; set; }
+
+        public double Height { get; set; }
+
+    }
+}
