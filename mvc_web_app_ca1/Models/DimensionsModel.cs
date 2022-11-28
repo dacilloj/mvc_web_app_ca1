7c03834d9db4d94c4b1166e4e4ae3f44ddc183be
dacillo <johncarlodacillo123@gmail.com>
2022-11-28 18:41:53 +0000

    Added dimensions model and some validations

---
+++ mvc_web_app_ca1/Models/DimensionsModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mvc_web_app_ca1.Models
+{
+    public class DimensionsModel
+    {
+        [Required]
+        public double Volume { get; set; }
+        [Required]
+        public double Density { get; set; }
+        [Required]
+        public enum ShippingCat
+        {
+            Low,
+            Medium,
+            High
+        }
+    }
+}
--- mvc_web_app_ca1/Models/PackageModel.cs
+++ mvc_web_app_ca1/Models/PackageModel.cs
@@ -1,17 +1,27 @@
-﻿namespace mvc_web_app_ca1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mvc_web_app_ca1.Models
 {
     public class PackageModel
     {
+        [Required]
         public int Id { get; set; }
-
+        [Required]
+        [StringLength(60)]
         public string ClientName { get; set; }
 
+        [Required]
+        [StringLength(200, MinimumLength =6)]
         public string Address { get; set; }
 
-        public double Weight { get; set; } 
+        [Required]
+        [Range(0,100)]
+        public double Weight { get; set; }
 
+        [Required]
+        [Range(0,200)]
         public double Lenght { get; set; }
-
+        [Required]
         public double Height { get; set; }
 
     }
