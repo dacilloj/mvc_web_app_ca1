753dda9ba447433eb87466d9c8fb6eed46a309d0
dacillo <johncarlodacillo123@gmail.com>
2022-11-28 18:32:26 +0000

    Add project files.

---
+++ mvc_web_app_ca1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvc_web_app_ca1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
